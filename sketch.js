f815fde7575149031ac5287b6dc0f096ab822c93
sbector <saulbector@gmail.com>
2018-03-03 14:05:22 -0600

    Inicialización de fechas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+var eventDate = new Date("2018-03-13T21:00:00");
+var currentDate;
+var PEventdate, PCurrentDate;
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	PEventdate = select('#eventDate');
+	PCurrentDate = select('#currentDate');
 }
 
 function draw() {
-
+	currentDate = new Date();
+	PEventdate.html(eventDate);
+	PCurrentDate.html(currentDate);
 }
