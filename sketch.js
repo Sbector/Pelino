e78123aa43faadb83ca06218aabc3739df518c28
sbector <saulbector@gmail.com>
2018-03-03 15:29:58 -0600

    Syncronización con UTC and Unix

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
 var eventDate = new Date("2018-03-03T14:15:00").toUTCString();
 var currentDate;
-var PEventdate, PCurrentDate;
+var offset;
+var storeDate;
+var syncDate;
+var PEventdate, PCurrentDate, PSyncDate;
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	PEventdate = select('#eventDate');
 	PCurrentDate = select('#currentDate');
+	PSyncDate = select('#syncDate');
 }
 
 function draw() {
+
 	currentDate = new Date().toUTCString();
+
+	offset = ServerDate - new Date();
+	storeDate = new Date(currentDate).getTime();
+	syncDate = new Date(storeDate-(offset)).toUTCString();
+	
 	PEventdate.html(eventDate);
 	PCurrentDate.html(currentDate);
+	PSyncDate.html(syncDate);
 }
